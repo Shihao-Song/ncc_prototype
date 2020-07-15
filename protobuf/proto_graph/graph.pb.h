00b22294717185dd3ce62cdc01a5356c47f61b96
baronj97 <jab644@drexel.edu>
2020-07-15 12:52:46 -0400

    Resolve potential typo causing compilation errors

--- protobuf/proto_graph/graph.pb.h
+++ protobuf/proto_graph/graph.pb.h
@@ -46,7 +46,7 @@ PROTOBUF_NAMESPACE_CLOSE
 struct TableStruct_graph_2eproto {
   static const ::PROTOBUF_NAMESPACE_ID::internal::ParseTableField entries[]
     PROTOBUF_SECTION_VARIABLE(protodesc_cold);
-  static const ::PROTOBUF_NAMESPACE_ID::internal::AuxillaryParseTableField aux[]
+  static const ::PROTOBUF_NAMESPACE_ID::internal::AuxiliaryParseTableField aux[]
     PROTOBUF_SECTION_VARIABLE(protodesc_cold);
   static const ::PROTOBUF_NAMESPACE_ID::internal::ParseTable schema[2]
     PROTOBUF_SECTION_VARIABLE(protodesc_cold);
